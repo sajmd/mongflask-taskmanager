3896c763a214f88a8a04caa96fb6734ceff43687
sajmd <sajidjava90@gmail.com>
2021-03-02 08:43:53 +0000

    Add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,19 @@
- $(document).ready(function () {
+$(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
-  });
+    $(".collapsible").collapsible();
+});
+
+
+
+
+
+/*
+    vanilla JavaScript for MaterializeCSS initialization
+*/
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     let sidenavs = document.querySelectorAll(".sidenav");
+//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
+// });
