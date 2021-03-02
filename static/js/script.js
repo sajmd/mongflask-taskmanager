0887c239ed73b4def15d7206435f7326351da08f
sajmd <sajidjava90@gmail.com>
2021-03-02 09:10:15 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,5 @@
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
     $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
 });
-
-
-
-
-
-/*
-    vanilla JavaScript for MaterializeCSS initialization
-*/
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     let sidenavs = document.querySelectorAll(".sidenav");
-//     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-//     let collapsibles = document.querySelectorAll(".collapsible");
-//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
-// });
